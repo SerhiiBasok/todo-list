eee3a0ddb9107359414523601127e6ac13985737
Serhii Basok <sergeybsk19@gmail.com>
2025-08-20 10:56:30 +0300

    create admin

--- app_list/apps.py
+++ app_list/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class AppListConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_list'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "app_list"
