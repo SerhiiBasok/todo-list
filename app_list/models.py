95fea26a36ca8a16382b24b3219e10766b7fd528
Serhii Basok <sergeybsk19@gmail.com>
2025-08-20 15:27:34 +0300

    create TODO list

--- app_list/models.py
+++ app_list/models.py
@@ -8,12 +8,15 @@ class Task(models.Model):
     is_complete = models.BooleanField(default=False)
     tags = models.ManyToManyField("Tag", related_name="tasks")
 
+    class Meta:
+        ordering = ['is_complete', '-created_at']
+
     def __str__(self):
         return self.content[:100]
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
